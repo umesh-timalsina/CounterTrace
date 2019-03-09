8a8de3070e27323e74a8f0de4d308602ba3f696c
Umesh Timalsina <umesh.timalsina@ioe.edu.np>
2019-03-08 20:58:54 -0600

    Add Code

--- ContourTracer.py
+++ ContourTracer.py
@@ -1,6 +1,8 @@
 import numpy as np
 from PIL import Image
 import logging
+from matplotlib import pyplot as plt
+
 
 
 class ContourTracer(object):
@@ -15,11 +17,14 @@ class ContourTracer(object):
         self.logger = logging.getLogger("ContourTracer")
         logging.basicConfig(level=logging.INFO,
                             format='%(asctime)s %(message)s')
-        im = Image.open(image_path).convert('L')
-        self.tess = np.asarray(im)
+        # im = Image.open(image_path).convert('L')
+        self.tess = np.fromfile(image_path)
+        self.tess = self.tess.reshape((64, 128))
         self.image_dims = self.tess.shape
         self.logger.info("Loaded Image from {0} as {1}*{2} numpy array"
-                            .format(image_path, self.tess.shape[0], self.tess.shape[1]))
+                            .format(image_path, self.tess.shape[0], self.tess.shape[0]))
+        plt.imshow(self.tess)
+        plt.show()
         pass
 
     def count_contours(self, pixel_intensity):
@@ -45,9 +50,8 @@ class ContourTracer(object):
         for i in range(self.image_dims[0]):
             for j in range(self.image_dims[1]):
                 if self.tess[i, j] == pixel_intensity and (i, j) not in self.already_checked:
-                    self.already_checked.append((i, j))
-                    if self.check_closed_contour((i, j), pixel_intensity):
-                        count += 1
+                    self.check_closed_contour((i, j), pixel_intensity)
+                    break
         return count
     
 
@@ -61,20 +65,28 @@ class ContourTracer(object):
         
         Returns:  
         """
+        print("Method Call")
         B = []
+        # self.already_checked.append(pixel_coordinates)
         B.append(pixel_coordinates)
         next_pixel = (-1, -1)
         next_pixel_boundaries = ContourTracer.moores_boundary(pixel_coordinates, self.image_dims)
-        while (pixel_coordinates[0] != next_pixel[0]) and (pixel_coordinates[1] != next_pixel[1]):
+        while pixel_coordinates != next_pixel:
+            print(pixel_coordinates, next_pixel)
+            # print(next_pixel_boundaries)
             for pixel in next_pixel_boundaries:
-                if self.tess[pixel[0], pixel[1]] == pixel_intensity:
+                next_pixel = pixel
+                if self.tess[pixel[0], pixel[1]] == pixel_intensity and pixel not in B[1:]:
                     self.already_checked.append(pixel)
+                    # print(self.already_checked)
                     B.append(pixel)
-                    next_pixel = pixel
                     break
-            if(pixel_coordinates[0] == next_pixel[0] and pixel_coordinates[1] == next_pixel[1]):
-                print(next_pixel)
+                if self.tess[pixel[0], pixel[1]] == pixel_intensity and pixel in B[1:]:
+                    continue
+                    # break
             next_pixel_boundaries =  ContourTracer.moores_boundary(next_pixel, self.image_dims)
+        else:
+             print("Broke from the pixel", next_pixel)
         return True
 
 
@@ -87,7 +99,7 @@ class ContourTracer(object):
         Arguments:
             -- pixel_coordinates: tuple (x, y) representing x and y coordinates 
             -- image_dims: tuple (width, height) representing image size
-        
+    
         Returns:
             -- moores_boundary: A List of tuples 
                     [(x-1, y-1), (x, y-1), (x+1, y-1), (x+1, y),
@@ -96,14 +108,14 @@ class ContourTracer(object):
 
         assert(isinstance(pixel_coordinates, tuple))
         x, y = pixel_coordinates
-        p_1 = (x-1, y-1)
-        p_2 = (x, y-1)
-        p_3 = (x+1, y-1)
-        p_4 = (x+1, y)
-        p_5 = (x+1 , y+1)
-        p_6 = (x, y+1)
-        p_7 = (x-1, y+1)
-        p_8 = (x-1, y)
+        p_1 = (x, y+1)
+        p_2 = (x-1, y+1)
+        p_3 = (x-1, y)
+        p_4 = (x-1, y-1)
+        p_5 = (x , y-1)
+        p_6 = (x+1, y-1)
+        p_7 = (x+1, y)
+        p_8 = (x+1, y+1)
         boundary_list = [p_1, p_2, p_3, p_4, p_5, p_6, p_7, p_8]
         moores_boundary = [ x for x in boundary_list if ((x[0] >= 0 and x[0] < image_dims[0])
                             and (x[1] >= 0 and x[1] < image_dims[1]))]
@@ -111,6 +123,6 @@ class ContourTracer(object):
 
 
 if __name__ == "__main__":
-    ct = ContourTracer("./images/shades-of-grey.png")
+    ct = ContourTracer("./images/sample.bin")
     print(ContourTracer.moores_boundary((3, 3), (640, 640)))
-    print(ct.count_contours(200))
+    print(ct.count_contours(0))
